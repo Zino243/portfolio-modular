1b5fab9b98cac918becdcd4e42bb7f3a1bf73144
Zino243 <bloodxs86@gmail.com>
2025-09-13 17:28:20 +0100

    agregando react a astro para incluir las astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve : {
+      alias: {
+        '@': '/src',
+      }
+    }
+  },
+
+  integrations: [react()]
 });
